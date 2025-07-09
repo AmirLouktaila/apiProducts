9a022c0194f80aba7bad05bfdd2d6fb8df2a12cd
Salah Louktaila <86385687+AmirLouktaila@users.noreply.github.com>
2025-07-09 17:50:27 +0300

    Add files via upload

---
+++ api_database.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const app = express();
+const database = require("./supabase_func");
+const productsTable = database('products');
+const categoryTable = database('category');
+PORT = 3000
+app.get('/products', async (_req, res) => {
+    try {
+        const products = await productsTable.getAllProducts();
+
+        if (products.length > 0) {
+            return res.send({
+                success: true,
+                products: products
+            });
+        }
+
+        return res.send({
+            success: false,
+            message: 'no products',
+            products: products
+        });
+
+    } catch (err) {
+        console.error('Error:', err.message);
+        return res.status(500).send({
+            success: false,
+            message: 'Internal server error'
+        });
+    }
+});
+
+app.get('/product', async (req, res) => {
+    const { id } = req.query;
+
+    try {
+        const product = await productsTable.prodcutID(id);
+
+        if (product.length > 0) {
+            return res.send({
+                success: true,
+                product: product
+            });
+        }
+
+        return res.send({
+            success: false,
+            message: 'No products found',
+            product: product
+        });
+
+    } catch (err) {
+        console.error('Error:', err.message);
+        return res.status(500).send({
+            success: false,
+            message: 'Internal server error',
+        });
+    }
+});
+app.get('/categorys', async (_req, res) => {
+    try {
+        const categorys = await categoryTable.getAllProducts();
+
+        if (categorys.length > 0) {
+            return res.send({
+                success: true,
+                categorys: categorys
+            });
+        }
+
+        return res.send({
+            success: false,
+            message: 'no products',
+            products: products
+        });
+
+    } catch (err) {
+        console.error('Error:', err.message);
+        return res.status(500).send({
+            success: false,
+            message: 'Internal server error'
+        });
+    }
+});
+
+app.post('/createProdcut', async (req, res) => { 
+    
+})
+app.listen(PORT, () =>
+    console.log(`🚀 http://localhost:${PORT} قيد التشغيل`)
+);
---
+++ supabase_func.js
@@ -0,0 +1,55 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(process.env.SB_URL, process.env.SB_KEY, { auth: { persistSession: false } });
+
+
+function database(tableName) {
+    return {
+        async createUser(user) {
+            const { data, error } = await supabase
+                .from(tableName)
+                .insert([user]);
+
+            if (error) {
+                throw new Error(`Error creating user: ${error.message}`);
+            }
+            return data;
+        },
+
+        async updateUser(id, update) {
+            const { data, error } = await supabase
+                .from(tableName)
+                .update(update)
+                .eq('id', id);
+
+            if (error) {
+                throw new Error(`Error updating user: ${error.message}`);
+            }
+            return data;
+        },
+
+        async prodcutID(userId) {
+            const { data, error } = await supabase
+                .from(tableName)
+                .select('*')
+                .eq('id', userId);
+
+            if (error) {
+                throw new Error(`Error fetching user: ${error.message}`);
+            }
+            return data;
+        },
+        async getAllProducts() {
+            const { data, error } = await supabase
+                .from(tableName)
+                .select('*');
+
+            if (error) {
+                throw new Error(`Error fetching products: ${error.message}`);
+            }
+            return data;
+        },
+    };
+}
+
+module.exports = database;
